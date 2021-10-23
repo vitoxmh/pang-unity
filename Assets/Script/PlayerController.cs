1edbda6bf1e70261ce69fb2f18ffc14671624e6e
Victor Morales <victor.morales@fusiona.cl>
2021-10-23 16:26:17 -0300

    ladder

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -18,18 +18,12 @@ public class PlayerController : MonoBehaviour
     public int typeArms;
     public int maxFire;
     public bool onLadder = false;
-    public float climbSpeed;
     public float exitHop = 3f;
     public bool Grounded;
     public Vector2 posLadder;
     private Sprite[] textures;
     private SpriteRenderer sr;
-    public int downLadder;
     public int maxShot;
-    public bool ladderFooter;
-    public bool ladderTop;
-    public bool ladderExit;
-    public bool ladderTopExit;
     private float detalDelayBang;
     public bool stateFreeze;
     private int nRebote;
@@ -54,11 +48,6 @@ public class PlayerController : MonoBehaviour
         Animator = GetComponent<Animator>();
         textures = Resources.LoadAll<Sprite>("Sprites/player/upladder");
         sr = GetComponent<SpriteRenderer>();
-        downLadder = 0;
-        ladderFooter = false;
-        ladderTop = false;
-        ladderExit = false;
-        ladderTopExit = false;
         detalDelayBang = Time.time;
         nRebote = 0;
 
@@ -137,181 +126,7 @@ public class PlayerController : MonoBehaviour
     {
 
 
-        if (ladderExit && vertical < 0 && ladderTop)
-        {
-            
-            Animator.SetInteger("PlayerAnimation", 5);
-
-            ladderExit = false;
-            return;
-
-        }
-
-
-        if (vertical > 0 && ladderTopExit)
-        {
-
-            Animator.SetInteger("PlayerAnimation", 5);
-
-
-            return;
-
-        }
-
-
-        if (ladderExit && vertical > 0 && ladderTopExit)
-        {
-
-            Animator.SetInteger("PlayerAnimation", 5);
-            ladderTopExit = false;
-
-            return;
-
-        }
-
-
-
-
-        if (ladderFooter)
-        {
-            if(vertical > 0)
-            {
-                transform.position = new Vector3(posLadder.x, transform.position.y + 0.05f, transform.position.z);
-                 
-               
-                Animator.SetInteger("PlayerAnimation", 3);
-            }
-            else
-            {
-                if(horizontal == 0)
-                {
-
-                    transform.Translate(0, 0, 0);
-                    Animator.SetInteger("PlayerAnimation", 0);
-                   
-
-                }
-                else if(horizontal < 0)
-                {
-                    if(wallLeft)
-                    {
-                        transform.Translate(0, 0, 0);
-                    }
-                    else
-                    {
-                        transform.Translate(-2 * Time.deltaTime * Speed, 0, 0);
-                    }
-                    
-                    Animator.SetInteger("PlayerAnimation", 1);
-
-                }
-                else if (horizontal > 0)
-                {
-                    if (wallRigth)
-                    {
-
-                        transform.Translate(0, 0, 0);
-                    }
-                    else
-                    {
-                        transform.Translate(2 * Time.deltaTime * Speed, 0, 0);
-                    }
-                   
-                    Animator.SetInteger("PlayerAnimation", 1);
-
-                }
-            }
-        }
-        else
-        {
-
-            if (onLadder)
-            {
-
-
-                if (vertical > 0)
-                {
-                    transform.Translate(0, 2 * Time.deltaTime * Speed, 0);
-                    Animator.SetInteger("PlayerAnimation", 3);
-
-                }
-                else if(vertical < 0)
-                {
-                    transform.Translate(0, -2 * Time.deltaTime * Speed, 0);
-                    Animator.SetInteger("PlayerAnimation", 3);
-
-                }
-                else if(vertical == 0)
-                {
-                    transform.Translate(0, 0, 0);
-                    Animator.SetInteger("PlayerAnimation", 4);
-                    rg.velocity = Vector2.zero;
-                }
-
-            }
-            else
-            {
-                
-                if (horizontal == 0)
-                {
-
-                    transform.Translate(0, 0, 0);
-                    Animator.SetInteger("PlayerAnimation", 0);
-
-                }
-                else if (horizontal < 0)
-                {
-                    if(wallLeft)
-                    {
-                        transform.Translate(0, 0, 0);
-                    }
-                    else
-                    {
-                        if (!fly && !onLadder) { 
-                            transform.Translate(-2 * Time.deltaTime * Speed, 0, 0);
-                            rg.gravityScale = 1;
-                        }
-                        else
-                        {
-                            transform.Translate(-2 * Time.deltaTime * 0.5f, 0, 0);
-                            rg.gravityScale = 3;
-                        }
-                    }
-                   
-                    Animator.SetInteger("PlayerAnimation", 1);
-
-                }
-                else if (horizontal > 0)
-                {
-
-
-                    if (wallRigth)
-                    {
-                        transform.Translate(0, 0, 0);
-                    }
-                    else
-                    {
-                        if (!fly && !onLadder) { 
-                            
-                            transform.Translate(2 * Time.deltaTime * Speed, 0, 0);
-                            rg.gravityScale = 1;
-
-                        } else{
-                            transform.Translate(2 * Time.deltaTime * 0.5f, 0, 0);
-                            rg.gravityScale = 3;
-                        }
-                    }
-
-                    
-                    Animator.SetInteger("PlayerAnimation", 1);
-
-                }
-
-
-            }
-
-
-        }
+        
 
 
     }
@@ -330,7 +145,7 @@ public class PlayerController : MonoBehaviour
         if (hit.collider != null)
         {
       
-            if (hit.collider.tag == "piso" || hit.collider.tag == "ladderTop" || hit.collider.tag == "block" || hit.collider.tag == "ladderFooter")
+            if (hit.collider.tag == "piso" || hit.collider.tag == "block")
             {
 
                 Grounded = true;
@@ -341,24 +156,6 @@ public class PlayerController : MonoBehaviour
             {
                 Grounded = false;
                
-            }
-
-
-            if(hit.collider.tag == "ladderTop" && vertical < 0.0f)
-            {
-       
-               
-                posLadder = hit.collider.gameObject.transform.position;
-                hit.collider.gameObject.GetComponentInChildren<BoxCollider2D >().enabled = false;
-
-                transform.position = new Vector3(posLadder.x, transform.position.y - 0.05f, transform.position.z);
-                ladderTop = true;
-
-
-            }
-            else
-            {
-
             }
 
 
@@ -439,48 +236,6 @@ public class PlayerController : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
 
-        if (other.tag == "ladder")
-        {
-            onLadder = true;
-            //posLadder = new Vector2(other.gameObject.transform.position.x + 0.05f, transform.position.y);
-            posLadder = other.gameObject.transform.position;
-
-            if(GameManager.gm.Lose)
-            {
-                rg.gravityScale = 3;
-       
-            }
-            else
-            {
-                rg.gravityScale = 0;
-
-           
-            }
-            
-
-
-            other.gameObject.transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = false;
-        }
-
-        if (other.tag == "ladderFooter")
-        {
-            ladderFooter = true;
-        }
-
-        if (other.tag == "ladderExit")
-        {
-
-            ladderExit = true;
-
-        }
-
-        if (other.tag == "ladderTopExit")
-        {
-
-            ladderTopExit = true;
-     
-
-        }
 
 
         if ((other.tag == "piso") && nRebote == 0)
@@ -510,39 +265,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.tag == "ladder")
-        {
-            onLadder = false;
-            rg.gravityScale = 1;
-
-            other.gameObject.transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = true;
-         
-        }
-
-
-        if (other.tag == "ladderExit")
-        {
-
-            ladderExit = false;
-
-        }
-
-
-
-        if (other.tag == "ladderFooter")
-        {
-            ladderFooter = false;
-        }
-
-
-        if (other.tag == "ladderTopExit")
-        {
-
-            ladderTopExit = false;
-
-
-        }
-
+       
 
         if (other.tag == "RebootStage")
         {
@@ -635,9 +358,9 @@ public class PlayerController : MonoBehaviour
 
 
 
-        if (col.gameObject.tag == "ladderTop" || col.gameObject.tag == "block" || col.gameObject.tag == "piso" || col.gameObject.tag == "blockVertical")
+        if (col.gameObject.tag == "block" || col.gameObject.tag == "piso" || col.gameObject.tag == "blockVertical")
         {
-            Debug.Log("En escalera");
+
 
             fly = false;
         }
@@ -661,9 +384,7 @@ public class PlayerController : MonoBehaviour
 
         }
 
-
-
-        if (col.gameObject.tag == "ladderTop" || col.gameObject.tag == "block" || col.gameObject.tag == "piso" || col.gameObject.tag == "blockVertical")
+        if (col.gameObject.tag == "block" || col.gameObject.tag == "piso" || col.gameObject.tag == "blockVertical")
         {
            
 
@@ -724,18 +445,6 @@ public class PlayerController : MonoBehaviour
 
 
     }
-
-
-
-
-
-    public void exitLadder()
-    {
-        ladderTopExit = false;
-    }
-
-
-
 
 
 
